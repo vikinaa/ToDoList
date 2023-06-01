fcec9324a2bf5c6b1f90ceee1af72089d4644258
vikto <vikto@DESKTOP-RQLFRVO>
2023-06-01 15:22:18 +0300

    sorts

---
+++ src/todo/Sorts.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package todo;
+
+/**
+ *
+ * @author vikto
+ */
+public interface Sorts {
+    
+    public abstract void sortByDate();
+    
+    public abstract void sortByImp();
+    
+    public abstract void sortByStatus();
+    
+    
+}
