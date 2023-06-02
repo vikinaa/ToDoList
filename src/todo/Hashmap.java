d1cdc32636f850dd62fafd50ab39ad6582f27c76
vikto <vikto@DESKTOP-RQLFRVO>
2023-06-02 21:48:31 +0300

    i

--- src/todo/Hashmap.java
+++ src/todo/Hashmap.java
@@ -44,6 +44,15 @@ public class Hashmap implements Sorts{
 
     }
     
+    public Node returnNode(int key){
+        Node curr=head.next;
+        while(curr!=null){
+            if(curr.getKey()==key) return curr;
+            curr=curr.next;
+        }
+       return null;
+    }
+    
 
     public void put(Node newN) {
        Node kl;
@@ -73,15 +82,18 @@ public class Hashmap implements Sorts{
     }
 
     public String cout(int key){ 
+        
         String note="";
         Node kl=head.next;
         while(kl!=null){
             if(kl.getKey()==key) {
+                
                 note+=kl.getValue();
                 note+="\n";
                 note=note+"Изпълненост: "+kl.getDone();
                 note+="\n";
                 note=note+"Важност: "+kl.getImp();
+                
                 //System.out.println(); //deadline
             }
             kl=kl.next;
@@ -93,18 +105,40 @@ public class Hashmap implements Sorts{
     
     public String sstring() {
         String text="";
+        int br=1;
         Node kl = head.next;
         while (kl!= null) { 
-            text+="\n";
+            text+="\n"+Integer.toString(br)+". ";
             text+=cout(kl.getKey());
             text+="\n";
             text+="------------------------------------------------------------------------";
+            br++;
             kl = kl.next;
         }
         return text;
 
     }
     
+     public void swapValue(Node a, Node b) { //a>b
+        int temp=a.getKey();
+        int temp2=b.getKey();
+        b.setKey(-20);
+        a.setKey(temp2);
+        b.setKey(temp);
+        int temp3=a.getImp();
+        int temp4=b.getImp();
+        a.setImp(temp4);
+        b.setImp(temp3);
+        boolean temp5=a.getDone();
+        boolean temp6=b.getDone();
+        a.setDone(temp6);
+        b.setDone(temp5);
+        String temp7=a.getValue();
+        String temp8=b.getValue();
+        a.setValue(temp8);
+        b.setValue(temp7);
+   
+    }
     
 
     @Override
@@ -114,15 +148,34 @@ public class Hashmap implements Sorts{
 
     @Override
     public void sortByImp() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        Node curr = head.next;
+        for (int i = 0; i < size() - 1; i++) {
+
+            for (int j = 0; j < size() -i-1; j++) {
+               
+                if (curr.getImp() > (curr.next).getImp()) {
+
+                    swapValue(curr.next, curr);
+                }
+                
+
+            }
+            curr = curr.next;
+            
+
+        }
+
     }
 
     @Override
     public void sortByStatus() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
+    }
 
-}
+    
+
+
 
     
     
