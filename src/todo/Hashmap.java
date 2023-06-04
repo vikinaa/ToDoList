8243c5a0d0f7059c496c70ccb57b9243a52a4860
vikto <vikto@DESKTOP-RQLFRVO>
2023-06-04 16:26:15 +0300

    йк

--- src/todo/Hashmap.java
+++ src/todo/Hashmap.java
@@ -4,12 +4,9 @@
  */
 package todo;
 
+public class Hashmap implements Sorts {
 
-public class Hashmap implements Sorts{
-    
-    
-     
-     Node head = new Node(); 
+    Node head = new Node();
 
     public int size() {  //raboti
         if (head.next == null) {
@@ -17,23 +14,23 @@ public class Hashmap implements Sorts{
         } else {
             Node currkletka = head.next;
             int size = 0;
-            while (currkletka!= null) {
+            while (currkletka != null) {
                 size++;
                 currkletka = currkletka.next;
 
             }
-         return size;
+            return size;
         }
     }
 
-    public boolean different(Node kl) { 
+    public boolean different(Node kl) {
         if (kl != head.next) {
             Node check = head.next;
-            while (check!= null) {
+            while (check != null) {
                 if (check.getKey() == kl.getKey()) {
                     return false;
                 }
-                
+
                 check = check.next;
             }
 
@@ -43,168 +40,194 @@ public class Hashmap implements Sorts{
         }
 
     }
-    
-    public Node returnNode(int key){
-        Node curr=head.next;
-        while(curr!=null){
-            if(curr.getKey()==key) return curr;
-            curr=curr.next;
+
+    public Node returnNode(int key) {
+        Node curr = head.next;
+        while (curr != null) {
+            if (curr.getKey() == key) {
+                return curr;
+            }
+            curr = curr.next;
         }
-       return null;
+        return null;
     }
-    
 
     public void put(Node newN) {
-       Node kl;
-       Node temp=newN;
-         if (head.next == null) {
-                kl = newN;
-                head.next = kl;
-            }
-         
-           else {
-             if(different(temp)){
-                kl=head.next;
-                while (kl.next!= null) {
-                   kl = kl.next;
+        Node kl;
+        Node temp = newN;
+        if (head.next == null) {
+            kl = newN;
+            head.next = kl;
+        } else {
+            if (different(temp)) {
+                kl = head.next;
+                while (kl.next != null) {
+                    kl = kl.next;
                 }
 
-                kl.next=newN;
-             }
-             else System.out.println("Key not available");
+                kl.next = newN;
+            } else {
+                System.out.println("Key not available");
             }
-      
+        }
 
-    } 
-    
-    public void clear(){  //raboti
-        head.next=null;
     }
-    
-    public String isItDone(Node a){
+
+    public void clear() {  //raboti
+        head.next = null;
+    }
+
+    public String isItDone(Node a) {
         String done;
-        if(a.getDone()==true) done="Изпълнена";
-        else done="Неизпълнена";
+        if (a.getDone() == true) {
+            done = "Изпълнена";
+        } else {
+            done = "Неизпълнена";
+        }
         return done;
     }
 
-    public String cout(int key){ 
-        
-        String note="";
-        Node kl=head.next;
-        while(kl!=null){
-            if(kl.getKey()==key) {
-                
-                note+=kl.getValue();
-                note+="\n";
-                note=note+"Изпълненост: "+isItDone(kl);
-                note+="\n";
-                note=note+"Важност: "+kl.getImp();
-                note+="\n";
-                note+="Срок: "+kl.getDeadline();
+    public String cout(int key) {
+
+        String note = "";
+        Node kl = head.next;
+        while (kl != null) {
+            if (kl.getKey() == key) {
+
+                note += kl.getValue();
+                note += "\n";
+                note = note + "Изпълненост: " + isItDone(kl);
+                note += "\n";
+                note = note + "Важност: " + kl.getImp();
+                note += "\n";
+                note += "Срок: " + kl.getDeadline();
                 //System.out.println(); //deadline
             }
-            kl=kl.next;
-            
+            kl = kl.next;
+
         }
         return note;
-        
-            }
-    
-     public void deleteNode(int key){
-        Node pr=head.next;
-        Node kl=pr.next;
-        if((head.next).getKey()==key){
-            head.next=head.next.next;
+
+    }
+
+    public void deleteNode(int key) {
+        Node pr = head.next;
+        Node kl = pr.next;
+        if ((head.next).getKey() == key) {
+            head.next = head.next.next;
         }
-        
-        while(kl!=null){
-            if(kl.getKey()==key){
-                pr.next=kl.next;
+
+        while (kl != null) {
+            if (kl.getKey() == key) {
+                pr.next = kl.next;
             }
-            pr=pr.next;
-            kl=kl.next;
+            pr = pr.next;
+            kl = kl.next;
         }
-    } 
-    
+    }
+
     public String sstring() {
-        String text="";
-        int br=1;
+        String text = "";
+        int br = 1;
         Node kl = head.next;
-        while (kl!= null) { 
-            text+="\n"+Integer.toString(br)+". ";
-            text+=cout(kl.getKey());
-            text+="\n";
-            text+="------------------------------------------------------------------------------------------------------------------------------------------------------";
+        while (kl != null) {
+            text += "\n" + Integer.toString(br) + ". ";
+            text += cout(kl.getKey());
+            text += "\n";
+            text += "--------------------------------------------------------------------------------------------------------------------------------------------------";
             br++;
             kl = kl.next;
         }
         return text;
 
     }
-    
-     public void swapValue(Node a, Node b) { //a>b
-        int temp=a.getKey();
-        int temp2=b.getKey();
+
+    public void swapValue(Node a, Node b) { //a>b
+        int temp = a.getKey();
+        int temp2 = b.getKey();
         b.setKey(-20);
         a.setKey(temp2);
         b.setKey(temp);
-        int temp3=a.getImp();
-        int temp4=b.getImp();
+        int temp3 = a.getImp();
+        int temp4 = b.getImp();
         a.setImp(temp4);
         b.setImp(temp3);
-        boolean temp5=a.getDone();
-        boolean temp6=b.getDone();
+        boolean temp5 = a.getDone();
+        boolean temp6 = b.getDone();
         a.setDone(temp6);
         b.setDone(temp5);
-        String temp7=a.getValue();
-        String temp8=b.getValue();
+        String temp7 = a.getValue();
+        String temp8 = b.getValue();
         a.setValue(temp8);
         b.setValue(temp7);
-        String temp9=a.getDeadline();
-        String temp10=b.getDeadline();
+        String temp9 = a.getDeadline();
+        String temp10 = b.getDeadline();
         a.setDeadline(temp10);
         b.setDeadline(temp9);
-   
+
     }
-    
 
     @Override
     public void sortByDate() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        Node curr=head.next;
+        Node temp = curr.next;
+      
+        for (int i = 0; i < size() - 1; i++) {
+
+            for (int j = 0; j < size() -1; j++) {
+               while(temp!=null){
+                if (Integer.parseInt(curr.getYear())>Integer.parseInt(temp.getYear()) ) { //2024>2023 
+                     swapValue(temp, curr);
+                     
+                }
+                else if (Integer.parseInt(curr.getYear())==Integer.parseInt(temp.getYear())){ //2024=2024
+                    if (Integer.parseInt(curr.getMonth())>Integer.parseInt(temp.getMonth())){ 
+                           swapValue(temp,curr);
+                       
+                    }
+                    else if(Integer.parseInt(curr.getMonth())==Integer.parseInt(temp.getMonth())){
+                         if(Integer.parseInt(curr.getDay())>Integer.parseInt(temp.getDay())){ 
+                            swapValue(temp,curr);
+                        }
+                    }
+                }
+              
+               temp=temp.next;
+               }
+               
+            }
+            
+            curr=curr.next;
+            temp=curr.next;
+            
+    }
     }
 
     @Override
     public void sortByImp() {
-        Node curr = head.next;
+       Node curr = head.next;
+       Node temp = curr.next;
+
         for (int i = 0; i < size() - 1; i++) {
 
-            for (int j = 0; j < size() -i-1; j++) {
-               
-                if (curr.getImp() > (curr.next).getImp()) {
+            for (int j = 0; j < size() -1; j++) {
+               while(temp!=null){
+                if (curr.getImp() > (temp).getImp()) {
 
-                    swapValue(curr.next, curr);
+                    swapValue(temp, curr);
                 }
-                
-
+              
+               temp=temp.next;
+               }
+               
             }
-            curr = curr.next;
+            
+            curr=curr.next;
+            temp=curr.next;
             
 
         }
 
     }
 
-    @Override
-    public void sortByStatus() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-    }
-
-    
-
-
-
-    
-    
-    
+}
